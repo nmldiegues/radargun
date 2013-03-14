be6036846d1ca6c0a015ce5e41156a3ccf67b82e
Nuno Diegues <diegues.nuno@gmail.com>
2013-03-14 17:42:12 +0000

    Adding TO.

--- auto-bench.sh
+++ auto-bench.sh
@@ -3,19 +3,24 @@
 nova list | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' > all_machines
 
 algs[1]="gmu"
-algs[2]="twm"
+algs[2]="twc"
 
 benchmarks[1]="ycsb"
-benchmarks[2]="vacation"
-benchmarks[3]="micro"
+benchmarks[2]="micro"
+benchmarks[3]="vacation"
 
 params[1]="50 5 8"
 params[2]="50 16"
 params[3]="50 5 8"
 
+totalorder[1]="false"
+totalorder[2]="true"
+
 mkdir auto-results;
 
-for benchmark in 1 2 3
+for to in 1 2
+do
+for benchmark in 2 3
 do
     for nodes in 2 4 #10 20 30 40 50 60 70 80
     do
@@ -25,10 +30,10 @@ do
             for alg in 1 2
             do
                 bash toggle_${algs[$alg]}
-                bash ${benchmarks[$benchmark]}-scripts/run-test.sh ${params[$benchmark]}
+                bash ${benchmarks[$benchmark]}-scripts/run-test.sh ${params[$benchmark]} ${totalorder[$to]}
                 cp results-radargun/test-result-results2/infinispan4_ispn_$nodes.csv auto-results/${benchmarks[$benchmark]}-${algs[$alg]}-$nodes-$attempt.csv
             done
         done
     done
 done
-
+done
