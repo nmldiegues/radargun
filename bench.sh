e79036c5cfda0e6a5a1caac944557f788440c501
Nuno Diegues <diegues.nuno@gmail.com>
2013-08-06 16:18:08 +0100

    Fixed bench.sh to detect errors

--- bench.sh
+++ bench.sh
@@ -14,30 +14,45 @@ optStr[1]="col"
 optStr[2]="ran"
 
 size[1]="1000"
-size[2]="10000"
-size[3]="100000"
-size[4]="100000"
+size[2]="1000"
+size[3]="10000"
+size[4]="10000"
+size[5]="100000"
+size[6]="100000"
 
 arity[1]="2"
-arity[2]="8"
+arity[2]="3"
 arity[3]="8"
 arity[4]="16"
+arity[5]="8"
+arity[6]="16"
 
-mkdir auto-results;
-
-    for nodes in 16
+echo "starting"
+    for attempt in 1 2
     do
-        tail -$nodes all_machines > /home/ndiegues/machines
-        for attempt in 1 
+	echo "going for attempt $attempt"
+        for nodes in 32
         do
-            for ro in 2
+        tail -$nodes all_machines > /home/ndiegues/machines
+
+		echo "going for nodes $nodes"
+            for ro in 1 2 3 4
             do
+		echo "going for ro $ro"
                 for opt in 1
                 do
-		for sa in 1 2 3 4
+		echo "going for opt $opt"
+		for sa in 1 2 5 6
 		do
-                bash btt-scripts/run-test.sh ${readPerc[$ro]} 60 ${size[$sa]} ${options[$opt]} ${arity[$sa]}
-                cp results-radargun/test-result-results2/infinispan4_ispn_$nodes.csv auto-results/$nodes-${readPerc[$ro]}-${optStr[$opt]}-${size[$sa]}-${arity[$sa]}-$attempt.csv
+		echo "going for sa $sa"
+		echo "bash btt-scripts/run-test.sh ${readPerc[$ro]} 60 ${size[$sa]} ${size[$sa]} ${options[$opt]} ${arity[$sa]}"
+                bash btt-scripts/run-test.sh ${readPerc[$ro]} 60 ${size[$sa]} ${size[$sa]} ${options[$opt]} ${arity[$sa]}
+                mv results-radargun/test-result-results2/infinispan4_ispn_$nodes.csv auto-results/$nodes-${readPerc[$ro]}-${optStr[$opt]}-${size[$sa]}-${arity[$sa]}-$attempt.csv
+		rc=$?
+		if [[ $rc != 0 ]] ; then
+		    echo "Error within: bash btt-scripts/run-test.sh ${readPerc[$ro]} 60 ${size[$sa]} ${size[$sa]} ${options[$opt]} ${arity[$sa]}"
+		    exit $rc
+		fi
 		done
                 done
             done
