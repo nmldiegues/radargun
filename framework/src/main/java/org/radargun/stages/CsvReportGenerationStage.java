a93232c510405cb0558f9adf50dd1f1b5093d62c
Nuno Diegues <nmld@ist.utl.pt>
2013-08-19 16:54:18 +0000

    Bla

--- framework/src/main/java/org/radargun/stages/CsvReportGenerationStage.java
+++ framework/src/main/java/org/radargun/stages/CsvReportGenerationStage.java
@@ -82,7 +82,7 @@ public class CsvReportGenerationStage extends AbstractMasterStage {
 
         List<Integer> slaveIndexes = new ArrayList<Integer>(results.keySet());
         Collections.sort(slaveIndexes);
-
+try {
         List<String> dataRow = new ArrayList<String>();
         for (Integer i : slaveIndexes) {
             Map<String, Object> reportPerSlave = results.get(i);
@@ -103,7 +103,9 @@ public class CsvReportGenerationStage extends AbstractMasterStage {
             writeRowToFile(dataRow);
             dataRow.clear();
         }
-
+} catch (Exception e) {
+	e.printStackTrace();
+}
         closeFile();
     }
 
